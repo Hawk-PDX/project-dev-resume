832f102e813f4ec4764d30344a0d49c3eee31e6a
Hawk-PDX <hawkpdx@yahoo.com>
2026-01-07 14:12:14 -0800

    troubleshooting certificate section

---
+++ src/data/myCerts.js
@@ -0,0 +1,37 @@
+export const myCerts = [
+    {
+        entity: 'Scrimba',
+        course: 'Learn React',
+        issue_date: '07-26-2025',
+        credential_url: 'https://scrimba.com/learn-react-c0e',
+        photo_url: '../../public/Learn_React_Cert.png'
+    },
+    {
+        entity: 'Scrimba',
+        course: 'Advanced React',
+        issue_date: '10-13-2025',
+        credential_url: 'https://scrimba.com/advanced-react-c02h',
+        photo_url: '../../public/Advanced_React_Cert.png'
+    },
+    {
+        entity: 'Scrimba',
+        course: 'JavaScript Deep Dive',
+        issue_date: '07-26-2025',
+        credential_url: 'https://scrimba.com/javascript-deep-dive-c0a',
+        photo_url: '../../public/JS_Deep_Dive_Cert.png'
+    },
+    {
+        entity: 'Scrimba',
+        course: 'Fullstack Developer Path',
+        issue_date: '12-29-2025',
+        credential_url: 'https://scrimba.com/fullstack-path-c0fullstack',
+        photo_url: '../../public/Fullstack_Path_Cert.png'
+    },
+    {
+        entity: 'Scrimba',
+        course: 'Data Structures and Algorithms',
+        issue_date: '01-08-2026',
+        credential_url: 'https://scrimba.com/data-structures-and-algorithms-c0shn6ckdm',
+        photo_url: '../../public/DSA_Cert.png'
+    }
+]
